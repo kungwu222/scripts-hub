9a9941cbbabbb6ccab746b6ee71a23cebf870b8a
Huiyan Zhang <142384122+kungwu222@users.noreply.github.com>
2026-09-20 16:14:22 +0800

    Update guaji.sh

--- guaji.sh
+++ guaji.sh
@@ -230,6 +230,7 @@ fi
 
 docker run -d \
     --restart=always \
+    -e DOCKER_API_VERSION=1.40 \
     --name watchtower \
     -v /var/run/docker.sock:/var/run/docker.sock \
     containrrr/watchtower \
